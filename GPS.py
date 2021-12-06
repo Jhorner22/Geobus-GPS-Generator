2f76e67bab7dc8ce46bbc99142cb2c200c73eecd
Jhorner22 <48147687+Jhorner22@users.noreply.github.com>
2021-12-06 11:11:44 -0600

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -2,19 +2,19 @@ import time
 import random
 
 
-def moveeast(long):
+def moveeast(lat, long):
     long += .00001
     return long
 
-def movenorth(lat):
+def movenorth(lat, long):
     lat += .0001
     return lat
 
-def movewest(long):
+def movewest(lat, long):
     long -= .0001
     return long
 
-def movesouth(lat):
+def movesouth(lat, long):
     lat -= .0001
     return lat
 
@@ -23,19 +23,19 @@ def move():
     long = -94.18507413935218
     while(True):
         while(long < -94.18062079836531):
-            long = moveeast(long)
+            long = moveeast(lat, long)
             ## WRITE LONG TO DB HERE
             time.sleep(500)
         while(lat < 36.05666733331742):
-            lat = movenorth(lat)
+            lat = movenorth(lat, long)
             ## WRITE LAT TO DB HERE
             time.sleep(500)
         while(long > -94.18504237244547):
-            long = movewest(long)
+            long = movewest(lat, long)
             ## WRITE LONG TO DB HERE
             time.sleep(500)
         while(lat > 36.04875525081654):
-            lat = movesouth(lat)
+            lat = movesouth(lat, long)
 
 def main():
     move()
